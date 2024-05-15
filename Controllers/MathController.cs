4d85b850f8f77b04cef8a3aa38af90a0d3e762af
edoardo pieri <pc@DESKTOP-9K38V74>
2024-05-15 08:43:39 +0200

    feat: volume cylinder

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -6,7 +6,7 @@ namespace AS2324_5G_INF_PieriEdoardo_WebAPI2.Controllers
     [Route("api/[controller]")]
     public class MathController : Controller
     {
-        [HttpGet("Celsius to Fahrenheit")]
+        [HttpGet("Celsius_To_Fahrenheit")]
         public JsonResult celsius_to_fahrenheit(float celsius)
         {
             if(celsius > -273)
@@ -17,6 +17,12 @@ namespace AS2324_5G_INF_PieriEdoardo_WebAPI2.Controllers
                 return Json(new { status = "KO", message = "temperatura in celsius errata" });
             }
 
+        }
+        [HttpGet("volume_cylinder")]
+        public JsonResult volume_cylinder(float radius, float height)
+        {
+            float c_area = radius * radius * (float)Math.PI;
+            return Json(new { status = "OK", result = c_area * height, message = "volume calcolato correttamente" });
         }
     }
 }
